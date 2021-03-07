af35d9d9f252f2d9baed9f43ef172fcbdca8a404
Volkoffx <58873963+volkoffvitaly@users.noreply.github.com>
2021-03-07 19:26:29 +0700

    init commit
    Based on .Net Core 5

---
+++ HITs-Website-2.0/HITs-Website-2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HITs_Website_2._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ HITs-Website-2.0/HITs-Website-2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HITs_Website_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
