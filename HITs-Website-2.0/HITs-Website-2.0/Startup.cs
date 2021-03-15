39847a71b446d39a8e53872ff95e670884ee421b
Volkoffx <58873963+volkoffvitaly@users.noreply.github.com>
2021-03-15 22:27:01 +0700

    Header is done (responsive ver.)
    Small bug in -sm- size with dropdown can be fixed later. Not interesting now.

--- HITs-Website-2.0/HITs-Website-2.0/Startup.cs
+++ HITs-Website-2.0/HITs-Website-2.0/Startup.cs
@@ -27,6 +27,9 @@ namespace HITs_Website_2._0
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages()
+                    .AddRazorRuntimeCompilation();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
