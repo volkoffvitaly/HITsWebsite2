28b93072d0d72b2c8c94a53b3db74bc08bf32704
ArtyomZavarzin <64472101+ArtyomZavarzin@users.noreply.github.com>
2021-03-20 20:17:02 +0700

    Changes for footer
    1) Footer become sticky
    2) The map has lost unnecessary buttons
    3) Change from px to rem/em
    4) Added adaptability for different screen resolutions

--- HITs-Website-2.0/HITs-Website-2.0/wwwroot/js/site.js
+++ HITs-Website-2.0/HITs-Website-2.0/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function YandexReadyHandler() {
+    var map = new ymaps.Map("ymap", {
+        center: [56.46910481095714, 84.94797934925934],
+        zoom: 16,
+        controls: [],
+        type: "yandex#map"
+    }, {
+        suppressMapOpenBlock: true
+    });
+    map.geoObjects.add(new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [56.46953536079353, 84.94760383999729],
+            hideIconOnBalloonOpen: false
+        },
+        properties: {
+            balloonContent: decodeURIComponent("%D0%A2%D0%BE%D1%87%D0%BA%D0%B0"),
+            iconCaption: decodeURIComponent(""),
+            hintCaption: decodeURIComponent(""),
+        }
+    }, {
+        preset: "islands#blueIcon",
+    })
+    ); return map;
+}
