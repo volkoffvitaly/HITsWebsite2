f1dc88f165dc83508bd1132f2ff0e694283124ae
ArtyomZavarzin <64472101+ArtyomZavarzin@users.noreply.github.com>
2021-04-02 04:36:58 +0700

    Navbar is fully functional
    Fixed: navbar does not overlap with footer

--- HITs-Website-2.0/HITs-Website-2.0/wwwroot/js/site.js
+++ HITs-Website-2.0/HITs-Website-2.0/wwwroot/js/site.js
@@ -5,14 +5,13 @@
 
 function AssignNavbarParameters() {
     navbarWidth = $navbarContainer.width()
-    startPoint = $("#blocks-container :header:first").first().offset().top
-    topIndent = parseInt($($navbarPlace).offset().top - $("#blocks-container :header:first").first().offset().top)
-
-    console.log(topIndent)
+    topIndent = parseInt($($navbarPlace).offset().top - $("#blocks-container .info-block:first").first().offset().top)
+    startPoint = $("#blocks-container .info-block:first").first().offset().top
+    finishPoint = $navbarContainer.height() + $navbarContainer.offset().top - $navbar.height() - topIndent
 }
 
 function ChangeToFixed() {
-    $navbar.removeClass("default").addClass("fixed");
+    $navbar.addClass("fixed").removeClass("default").removeClass("press-down");
     $navbar.css('top', `${topIndent}px`)
     $navbar.css('width', `${navbarWidth}px`)
 }
@@ -23,56 +22,106 @@ function ChangeToDefault() {
     $navbar.css('width', ``)
 }
 
+function ChangeToPressDown() {
+    $navbar.css('top', ``)
+    $navbar.removeClass("fixed").addClass("press-down");
+}
+
+function GetPostionTop(elem) {
+    return $(elem).offset().top - $(this).scrollTop()
+}
+
+function SelectCurrentNavbarItem() {
+    let currElem = $(".info-block > a").first()
+
+    $(".info-block > a").each(function (index, elem) {
+        if (GetPostionTop(elem) <= 1) {
+            currElem = elem
+        }
+    })
+
+    $navbar.find(".side-navbar-selected").removeClass("side-navbar-selected")
+    let sc = $(currElem).attr("name")
+    $(`a[href*='${sc}']`).first().parent().addClass("side-navbar-selected")
+}
+
 var $navbar = $("#side-navbar");
 var $navbarContainer = $("#side-navbar-container");
 var $navbarPlace = $("#side-navbar-place");
 
 var navbarWidth
 var startPoint
+var finishPoint
 var topIndent
 
-AssignNavbarParameters()
-
 $(window).scroll(function () {
-    if ($(this).scrollTop() > startPoint && $navbar.hasClass("default")) {
-        ChangeToFixed()      
-    } else if ($(this).scrollTop() <= startPoint && $navbar.hasClass("fixed")) {
+
+    if ($(this).scrollTop() <= startPoint && $navbar.hasClass("fixed")) {
         ChangeToDefault()
+    } else if ($(this).scrollTop() > startPoint && $(this).scrollTop() < finishPoint && ($navbar.hasClass("default") || $navbar.hasClass("press-down"))) {
+        ChangeToFixed()
+    } else if ($(this).scrollTop() >= finishPoint && $navbar.hasClass("fixed")) {
+        ChangeToPressDown()
+    }    
+
+    if ($navbar.hasClass("inAnimState")) {
+        return true
     }
 
-
+    SelectCurrentNavbarItem()
 });//scroll
 
 $(window).resize(function () {
     AssignNavbarParameters()
 
-    if ($(this).scrollTop() > startPoint) {
+    if ($(this).scrollTop() > startPoint && $(this).scrollTop() < finishPoint) {
         ChangeToFixed()
     } else if ($(this).scrollTop() <= startPoint) {
         ChangeToDefault()
+    } else {
+        ChangeToPressDown()
     }
 });//resize
 
 $(document).ready(function () {
 
+    //Добавляем пункты в навбар
     $(".info-block").each(function (index, elem) {
-
         let title = $(elem).find("h2:first")
         $(title).before(`<a name="#${index}"></a>`)
-
         $navbar.find("ul").append(`<li><div><a href="#${index}">${$(title).text()}</a></div></li>`)
     })
 
+    //Делаем переход по якорям плавным
     $navbar.on("click", "a", function (event) {
         event.preventDefault();
-        var sc = $(this).attr("href")
-        var dn = $(`a[name*='${sc}']`).first().offset().top;
-        $('html, body').animate({ scrollTop: dn - 20 }, 700);
+        let sc = $(this).attr("href")
+        let dn = $(`a[name*='${sc}']`).first().offset().top
+        let animationTime = 700;
+        $('html, body').animate({ scrollTop: dn }, animationTime)
+        $navbar.addClass("inAnimState")
+        setTimeout('$navbar.removeClass("inAnimState")', animationTime)
+        $navbar.find(".side-navbar-selected").removeClass("side-navbar-selected")
+        $(this).parent().addClass("side-navbar-selected")
     });
 
+    //Выставляем позицию навбара
+    AssignNavbarParameters()
+    if ($(this).scrollTop() > startPoint && $(this).scrollTop() < finishPoint) {
+        ChangeToFixed()
+    } else if ($(this).scrollTop() <= startPoint) {
+        ChangeToDefault()
+    } else {
+        ChangeToPressDown()
+    }
+
+    //Выделяем в навбаре наблюдаемый блок
+    SelectCurrentNavbarItem()
 });
 
 
+
+//Функция для выставление позици в Яндекс Картах и удаления элементов меню
 function YandexReadyHandler() {
     var map = new ymaps.Map("ymap", {
         center: [56.46910481095714, 84.94797934925934],
