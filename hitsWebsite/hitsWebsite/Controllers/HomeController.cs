102c2416ef5a3667890bf9ec3cb277eb1ba37a4b
Volkoffx <58873963+volkoffvitaly@users.noreply.github.com>
2021-04-26 19:35:37 +0700

    Language dropdown was fixed
    + Names of language don't translate more.
    + Default or selected <option> in <select> right aligned

--- hitsWebsite/hitsWebsite/Controllers/HomeController.cs
+++ hitsWebsite/hitsWebsite/Controllers/HomeController.cs
@@ -14,23 +14,6 @@ namespace hitsWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [HttpPost]
         public IActionResult CultureManagement(LanguageEditModel model, string returnUrl)
         {
@@ -38,12 +21,6 @@ namespace hitsWebsite.Controllers
                 new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
 
             return LocalRedirect(returnUrl);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
